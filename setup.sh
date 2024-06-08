15e27aac247d9301e1726b6d3923f8050eea7f75
Lukasz Sroka <me@lsroka.dev>
2024-06-08 20:23:38 +0200

    Add Finicky to setup and its config

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ brew install neovim
 brew install tmux
 brew install ripgrep
 brew install direnv
+brew install finicky
 
 brew install openssl readline sqlite3 xz zlib tcl-tk
 curl https://pyenv.run | bash
