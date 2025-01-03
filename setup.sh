a0489502b96ce1a231c76eb0225fdba78aa8c2c2
Lukasz Sroka <me@lsroka.dev>
2025-01-03 12:04:44 +0100

    Add obisidian and slides

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ brew install tmux
 brew install ripgrep
 brew install direnv
 brew install finicky
+brew install slides
+
+brew install --cask obsidian
 
 brew install openssl readline sqlite3 xz zlib tcl-tk
 curl https://pyenv.run | bash
